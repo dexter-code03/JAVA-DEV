7ffee88cc0e533d50930961ad9bd83e362a926d5
dexter-code03 <21cse078glbitm.ac.in>
2025-07-26 17:22:02 +0530

    Include varNdataType folder into JAVA DEV repo

---
+++ varNdataType/src/VariableDatatype.java
@@ -0,0 +1,17 @@
+public class VariableDatatype {
+    public static void main(String[] args) {
+        // Declare variables of different data types
+        int integerVar = 42; // Integer type
+        double doubleVar = 3.14; // Double type
+        float floatVar = 3.5f;
+        char charVar = 'A'; // Character type
+        boolean booleanVar = true; // Boolean type
+
+        // Print the values of the variables
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Float: " + floatVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("Boolean: " + booleanVar);
+    }
+}
