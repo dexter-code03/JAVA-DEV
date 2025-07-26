433df89b70eda0d3a34f877030b7bd747808b616
dexter-code03 <21cse078glbitm.ac.in>
2025-07-26 20:06:41 +0530

    Done

--- MethodOverloading/src/MethodOverloading.java
+++ MethodOverloading/src/MethodOverloading.java
@@ -1,5 +1,24 @@
+class TestOverLoad
+{
+    final private int a;
+
+    TestOverLoad(int a)
+    {
+        this.a=a;
+    }
+    //You can create method with same name just parameters should be different
+    public int sum(int b,int c)
+    {
+        return a+b+c;
+    }
+    public int sum(int b,int c,int d)
+    {
+        return a+b+c+d;
+    }
+}
 public class MethodOverloading {
     public static void main(String[] args) {
-        System.out.println("hellooo");
+        TestOverLoad demo = new TestOverLoad(10);
+        System.out.println("Method Overloading:\n"+"Sum:"+demo.sum(2,6)+"\n"+"Sum:"+demo.sum(2,6,9));
     }
 }
