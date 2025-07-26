e214140e2a4c629a4dc51cfbdeb07b6798539cb5
dexter-code03 <21cse078glbitm.ac.in>
2025-07-26 22:41:20 +0530

    DONE

---
+++ Array/src/Array.java
@@ -0,0 +1,32 @@
+
+
+public class Array {
+    public static void main(String[] args) {
+        int[] num = new int[]{2,3,4,5,6,7,8};
+        for(int i:num)
+        {
+            System.out.print(i+"\t");
+        }
+        System.out.print("""
+
+
+                """);
+        int[][] arr = new int[8][8];
+        for(int i = 0;i< arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                arr[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        for(int[] i:arr)
+        {
+            for (int j:i)
+            {
+                System.out.print(j+"\t");
+            }
+            System.out.print('\n');
+        }
+
+    }
+}
